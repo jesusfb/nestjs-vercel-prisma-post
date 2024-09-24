8cdfb6706115f2600086ae16bbd00ec2f3822eee
jesusfb <jfigueroa.beltran@gmail.com>
2024-09-24 15:55:04 -0700

    Update migration.sql

--- prisma/migrations/20211004163457_init/migration.sql
+++ prisma/migrations/20211004163457_init/migration.sql
@@ -3,34 +3,20 @@ CREATE TABLE "Post" (
     "id" SERIAL NOT NULL,
     "content" TEXT,
     "title" TEXT NOT NULL,
-    "authorId" INTEGER,
+    "authorId" INTEGER
 
-    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
-);
+  );
 
 -- CreateTable
 CREATE TABLE "Profile" (
     "id" SERIAL NOT NULL,
     "bio" TEXT,
-    "userId" INTEGER NOT NULL,
-
-    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+    "userId" INTEGER NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
-    "email" TEXT NOT NULL,
-    "name" TEXT,
+    "email" TEXT NOT NULL
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
